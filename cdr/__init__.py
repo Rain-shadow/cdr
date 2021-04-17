b05ef08a77367de49d34cfdeb3aa345e53f93294
Rain-shadow <shangyun07@gmail.com>
2021-04-17 22:40:03 +0800

    update __init__.py

--- cdr/__init__.py
+++ cdr/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#@Time  : 2020-12-19, 0019 15:30
-#@Author: 佚名
-#@File  : __init__.py.py
+# @Time  : 2020-12-19, 0019 15:30
+# @Author: 佚名
+# @File  : __init__.py.py
 from .login import Login
 from .core import do_homework
-__version__ = '1.10-beta'
+
+__version__ = '1.10.0'
 __author__ = 'Fuck CDR!'
 __all__ = ["Login", "do_homework", "__version__"]
