25f7ab3be38741d28360d07862e76ef92db9c5b5
Rain-shadow <shangyun07@gmail.com>
2021-04-17 14:41:41 +0800

    异步展示验证码

--- cdr/thread/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# cython : language_level=3
-# @Time  : 2020-12-29, 0029 15:26
-# @Author: 佚名
-# @File  : __init__.py.py
-from .custom_thread import CustomThread
-
-__all__ = ["CustomThread"]
--- cdr/utils/verification_code.py
+++ cdr/utils/verification_code.py
@@ -4,8 +4,12 @@
 # @Time  : 2021-01-15, 0015 21:38
 # @Author: 佚名
 # @File  : verification_code.py
+import asyncio
 import base64
 import io
+import os
+from asyncio.subprocess import SubprocessStreamProtocol
+
 from PIL import Image
 from cdr.config import CONFIG_DIR_PATH
 
@@ -13,11 +17,19 @@ from cdr.config import CONFIG_DIR_PATH
 class VerificationCode:
 
     @staticmethod
-    def get_vc(img_base64: str, task_id):
+    async def get_vc(img_base64: str, task_id):
         img_b64decode = base64.b64decode(img_base64)  # base64解码
 
         image = io.BytesIO(img_b64decode)
         img = Image.open(image)
         img.save(f"{CONFIG_DIR_PATH}验证码-{task_id}.png")
-        img.show()
-        return input(f"验证码-{task_id}：")
+        print(f"start {CONFIG_DIR_PATH}验证码-{task_id}.png")
+        loop = asyncio.get_event_loop()
+        # protocol_factory = lambda: SubprocessStreamProtocol(limit=2 ** 16,
+        #                                                     loop=loop)
+        # await loop.subprocess_shell(protocol_factory, "pause")
+        proc = await asyncio.create_subprocess_shell(f"start {CONFIG_DIR_PATH}验证码-{task_id}.png", loop=loop)
+        await proc.communicate()
+        code = input(f"验证码-{task_id}：")
+        os.remove(f"{CONFIG_DIR_PATH}验证码-{task_id}.png")
+        return code
